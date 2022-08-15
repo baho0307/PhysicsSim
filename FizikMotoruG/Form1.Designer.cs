81c718d7ef51548a250d3ee8d0895ec078df8c15
Bahadir Yigit <88906685+baho0307@users.noreply.github.com>
2022-08-15 21:41:24 +0300

    Update Form1.Designer.cs

--- FizikMotoruG/Form1.Designer.cs
+++ FizikMotoruG/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace FizikMotoruG
+namespace FizikMotoruG
 {
     partial class Form1
     {
@@ -30,6 +30,8 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -52,16 +54,28 @@
             this.label1.Text = "Distance:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(1337, 574);
+            this.trackBar1.Maximum = 11;
+            this.trackBar1.Minimum = -10;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(222, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1630, 772);
+            this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -71,5 +85,6 @@
 
         private Button button1;
         private Label label1;
+        private TrackBar trackBar1;
     }
 }
