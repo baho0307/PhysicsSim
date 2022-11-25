07bac80097932756890622b6c42e95d5abc09ee0
Bahadir Yigit <bahadiryigit0307@gmail.com>
2022-11-25 20:04:02 +0300

    Collision mechanics added

--- FizikMotoruG/Form1.cs
+++ FizikMotoruG/Form1.cs
@@ -7,41 +7,57 @@ namespace FizikMotoruG
             InitializeComponent();
 
         }
-        
 
-        public void Show(System.Drawing.Graphics grp,Pen mp,params Object[] obj)
+
+        public void Show(System.Drawing.Graphics grp, Pen mp, params Object[] obj)
         {
-            foreach(Object o in obj)
+            foreach (Object o in obj)
             {
-                grp.DrawEllipse(mp, (int)(o.transform.position.x + 500 - o.transform.scale / 2), (int)(o.transform.position.y + 500 - o.transform.scale / 2), (int)o.transform.scale, (int)o.transform.scale);
+                grp.DrawEllipse(mp, (int)(o.transform.position.x + 800 - o.transform.scale / 2), (int)(o.transform.position.y + 350 - o.transform.scale / 2), (int)o.transform.scale, (int)o.transform.scale);
+            }
+        }
+        public void ShowObj(Object obj)
+        {
+            foreach (Object o in Object.universe)
+            {
+                o.physics.v.x -= obj.physics.v.x;
+                o.physics.v.y -= obj.physics.v.y;
+                o.transform.position.x -= obj.transform.position.x;
+                o.transform.position.y -= obj.transform.position.y;
             }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-           
+
             System.Drawing.Graphics grp;
             Pen mp = new Pen(System.Drawing.Color.Green, 1);
+            Pen mp1 = new Pen(System.Drawing.Color.Red, 1);
             grp = this.CreateGraphics();
 
-            Object objA = new Object(new Physics(0, 20000000000000), new Transform(new Vector2(500, -500), 100));
-            Object objC = new Object(new Physics(0, 20000000000000), new Transform(new Vector2(50, -50), 100));
-            Object objB = new Object(new Physics(0, 20000000000000), new Transform(new Vector2(0, 0), 100));
+            Object objA = new Object(new Physics(0, 200000000000), new Transform(new Vector2(100, -100), 100));
+            //Object objC = new Object(new Physics(0, 20000000000000), new Transform(new Vector2(10, -100), 10));
+            Object objB = new Object(new Physics(0, 200000000000), new Transform(new Vector2(600, 100), 100));
             objA.physics.v.x = 0;
             objA.physics.v.y = 0;
             objB.physics.v.x = 0;
             objB.physics.v.y = 0;
-            objC.physics.v.x = 0;
-            objC.physics.v.y = 0;
-            Object.universe = new Object[]{objA,objB,objC};
+            //objC.physics.v.x = 0;
+            //objC.physics.v.y = 0;
+            Object.universe = new Object[] { objA, objB };
 
             do
             {
-                Show(grp,mp,Object.universe);
+                Application.DoEvents();
+                Show(grp, mp, objA);
+                Show(grp, mp1, objB);
+
+                objA.Calc();
+                objB.Calc();
 
                 objA.Go();
                 objB.Go();
-                objC.Go();
+
                 Thread.Sleep(Math.Abs(Time.deltaTime));
                 grp.Clear(Color.White);
             } while (true);
@@ -58,6 +74,6 @@ namespace FizikMotoruG
 
         }
 
-        
+
     }
 }
--- FizikMotoruG/PhysicsLib.cs
+++ FizikMotoruG/PhysicsLib.cs
@@ -1,5 +1,7 @@
+using System.Security.Cryptography;
+
 public class Vector2
-{   
+{
     public double x;
     public double y;
     public Vector2(double x, double y)
@@ -7,9 +9,9 @@ public class Vector2
         this.x = x;
         this.y = y;
     }
-    public static double Distance(Vector2 a,Vector2 b)
+    public static double Distance(Vector2 a, Vector2 b)
     {
-        return Math.Sqrt(Math.Pow((a.x - b.x),2) + Math.Pow((a.y - b.y),2));
+        return Math.Sqrt(Math.Pow((a.x - b.x), 2) + Math.Pow((a.y - b.y), 2));
     }
 }
 
@@ -45,44 +47,50 @@ public class Physics
     public Vector2 v;
     public Vector2 a;
     public Vector2 n;
+    public Vector2 nextN;
+    public Vector2 nextV;
     public Vector2 rot;
-    public const double G = .0000000000667 ;
+    public const double G = .0000000000667;
     public Physics(double velocity, double mass)
     {
         this.velocity = velocity;
         this.mass = mass;
         this.v = new Vector2(0, 0);
         this.a = new Vector2(0, 0);
+        this.n = new Vector2(0, 0);
+        this.nextN = new Vector2(0, 0);
+        this.nextV = new Vector2(0, 0);
     }
-    
-    public static Object IsColliding(Object self,params Object[] objects)
+
+    public static Object[] IsColliding(Object self, params Object[] objects)
     {
-        foreach(Object obj in objects)
+        List<Object> coll = new List<Object>();
+        foreach (Object obj in objects)
         {
-            if(Vector2.Distance(self.transform.position, obj.transform.position) <= (self.transform.scale + obj.transform.scale) / 2 && self != obj)
+            if (Vector2.Distance(self.transform.position, obj.transform.position) <= (self.transform.scale + obj.transform.scale) / 2 && self != obj)
             {
-                return obj;
+                coll.Add(obj);
             }
         }
-        return null;
+        return coll.ToArray();
     }
 
     public double Velocity()
     {
-        return Math.Sqrt(Math.Pow(this.v.x, 2) + Math.Pow(this.v.y,2));
+        return Math.Sqrt(Math.Pow(this.v.x, 2) + Math.Pow(this.v.y, 2));
     }
 
     public static void Gravity(params Object[] objects)
     {
-        foreach(Object _obj in objects)
+        foreach (Object _obj in objects)
         {
-            foreach(Object obj in objects)
+            foreach (Object obj in objects)
             {
-                if(_obj != obj && Physics.IsColliding(_obj, objects) == null)
+                if (_obj != obj && Physics.IsColliding(_obj, objects) == null)
                 {
-                    obj.physics.newton = G*(obj.physics.mass * _obj.physics.mass / Math.Pow(Vector2.Distance(obj.transform.position, _obj.transform.position), 2));
+                    obj.physics.newton = G * (obj.physics.mass * _obj.physics.mass / Math.Pow(Vector2.Distance(obj.transform.position, _obj.transform.position), 2));
                     double acceleration = obj.physics.newton / _obj.physics.mass;
-                    Object.GoToObj(_obj,obj,acceleration );
+                    Object.GoToObj(_obj, obj, acceleration);
                     _obj.physics.velocity = acceleration;
                 }
             }
@@ -104,12 +112,12 @@ public class Object
 
     public void Acceleration()
     {
+        this.physics.n.x = this.physics.nextN.x;
+        this.physics.n.y = this.physics.nextN.y;
         this.physics.a.x = this.physics.n.x / this.physics.mass;
         this.physics.a.y = this.physics.n.y / this.physics.mass;
         this.physics.v.x += this.physics.a.x;
-        this.physics.a.x = 0;
         this.physics.v.y += this.physics.a.y;
-        this.physics.a.y = 0;
     }
 
     public void GravityForce()
@@ -121,34 +129,44 @@ public class Object
             if (obj != this)
             {
                 Vector2 trgRot = new Vector2(obj.transform.position.x - this.transform.position.x, obj.transform.position.y - this.transform.position.y);
-                gSum.x += (trgRot.x/Math.Abs(trgRot.y)) * (Physics.G * this.physics.mass * obj.physics.mass / Math.Pow(Vector2.Distance(this.transform.position,obj.transform.position), 2));
-                gSum.y += (trgRot.y / Math.Abs(trgRot.y)) * (Physics.G * this.physics.mass * obj.physics.mass / Math.Pow(Vector2.Distance(this.transform.position, obj.transform.position), 2));
+                gSum.x += (trgRot.x / Math.Abs(trgRot.y)) * (Physics.G * this.physics.mass * obj.physics.mass / Vector2.Distance(this.transform.position, obj.transform.position));
+                gSum.y += (trgRot.y / Math.Abs(trgRot.y)) * (Physics.G * this.physics.mass * obj.physics.mass / Vector2.Distance(this.transform.position, obj.transform.position));
             }
         }
-        this.physics.n = gSum;
+        this.physics.nextN = gSum;
     }
-    public void Go()
+    public void Calc()
     {
-        if (Physics.IsColliding(this, Object.universe) == null)
+        if (Physics.IsColliding(this, Object.universe).Length == 0)
         {
             this.GravityForce();
-            this.Acceleration();
-            this.transform.position.x += this.physics.v.x / Time.deltaTime;
-            this.transform.position.y += this.physics.v.y / Time.deltaTime;
         }
         else
         {
-            this.physics.n.x += -this.physics.v.x * this.physics.mass;
-            this.physics.n.y += -this.physics.v.y * this.physics.mass;
-            this.Acceleration();
-            this.transform.position.x += this.physics.v.x / Time.deltaTime;
-            this.transform.position.y += this.physics.v.y / Time.deltaTime;
+            this.GravityForce();
+            Object[] collide = Physics.IsColliding(this, Object.universe);
+            foreach (Object obj in collide)
+            {
+                this.physics.v.x = (((this.physics.mass - obj.physics.mass) / (this.physics.mass + obj.physics.mass) * this.physics.Velocity()) +
+                    (2 * obj.physics.mass / (this.physics.mass + obj.physics.mass) * obj.physics.Velocity()));
+
+                this.physics.v.y = (((this.physics.mass - obj.physics.mass) / (this.physics.mass + obj.physics.mass) * this.physics.Velocity()) +
+                    (2 * obj.physics.mass / (this.physics.mass + obj.physics.mass) * obj.physics.Velocity()));
+            }
+
         }
     }
-    public static void GoTo(Object self ,Vector2 target,double a,params Object[] obj)
+    public void Go()
+    {
+        this.Acceleration();
+
+        this.transform.position.x += this.physics.v.x / Time.deltaTime;
+        this.transform.position.y += this.physics.v.y / Time.deltaTime;
+    }
+    public static void GoTo(Object self, Vector2 target, double a, params Object[] obj)
     {
         Vector2 trgRot = new Vector2(target.x - self.transform.position.x, target.y - self.transform.position.y);
-        
+
         for (int i = 0; i < Math.Abs(trgRot.x); i++)
         {
             if (Physics.IsColliding(self, obj) == null)
@@ -165,15 +183,15 @@ public class Object
             }
         }
     }
-    public static void GoToObj(Object self,Object target,double a)
+    public static void GoToObj(Object self, Object target, double a)
     {
         Vector2 trgRot = new Vector2(target.transform.position.x - self.transform.position.x, target.transform.position.y - self.transform.position.y);
         self.physics.rot = trgRot;
         for (int i = 0; i < Math.Abs(trgRot.x); i++)
         {
-            if(Physics.IsColliding(self, target) == null)
+            if (Physics.IsColliding(self, target) == null)
             {
-                self.transform.position.x += trgRot.x < 0 ? -1 * a / Time.deltaTime : 1 * a/Time.deltaTime;
+                self.transform.position.x += trgRot.x < 0 ? -1 * a / Time.deltaTime : 1 * a / Time.deltaTime;
             }
         }
 
